4b7fe1339ad84a98714fdccb4a36a9783d568661
Roman Njoroge <roman.njoroge@njuguna.com>
2022-07-05 22:01:42 +0300

    First nodeJS code

---
+++ first.js
@@ -0,0 +1,11 @@
+// Variables
+const amount = 12
+
+// Branching
+if (amount  < 10){
+    console.log('Small number')
+}else if(amount > 10){
+    console.log('Large number')
+}else{
+    console.log('This should never be reached')
+}
