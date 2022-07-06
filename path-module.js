fdf537eebbe9434b9d323b1dafa9bf4dbc867552
Roman Njoroge <roman.njoroge@njuguna.com>
2022-07-06 23:35:56 +0300

    Path module NodeJS

---
+++ path-module.js
@@ -0,0 +1,22 @@
+
+/*
+    The Path module allows us to interact with the file path
+*/
+const path = require('path')
+
+// System specific seperator
+const sep = path.sep
+console.log(sep)
+
+// Getting a file path
+const testFilePath = path.join('/test/', 'test-sub')
+console.log(testFilePath)
+
+// Getting base file name
+
+const base = path.basename(testFilePath)
+console.log(base)
+
+// Getting absolute path
+const absolute = path.resolve(__dirname, 'test', 'test-sub', 'test.txt')
+console.log(absolute)
