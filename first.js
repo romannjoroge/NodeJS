e18fff1ee06ed9bb7d1adc8f9be05ebee2e3e124
Roman Njoroge <roman.njoroge@njuguna.com>
2022-07-06 21:26:24 +0300

    Added array and object

--- first.js
+++ first.js
@@ -1,6 +1,16 @@
 // Variables
 const amount = 12
 
+// Objects
+const person = {
+    name: "Roman",
+    ID: "3976172",
+    age: 19
+}
+
+// Arrays
+const food = ['banana', 'ugali', 'pilau']
+
 // Branching
 if (amount  < 10){
     console.log('Small number')
