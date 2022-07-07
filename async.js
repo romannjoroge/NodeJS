71a0c35f824e5e1a5209c5087f83ec92837907a9
Roman Njoroge <roman.njoroge@njuguna.com>
2022-07-07 18:42:21 +0300

    How async works in NodeJS

---
+++ async.js
@@ -0,0 +1,12 @@
+console.log('first')
+setTimeout(()=>{
+    console.log('second')
+}, 0)
+console.log('third')
+setTimeout(()=>{
+    console.log('fourth')
+}, 0)
+console.log('fifth')
+for (var i = 0; i < 10; i++){
+    console.log(i)
+}
