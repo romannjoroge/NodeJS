39c4ad5d5dc980cb02a0bd5ab30b21694a7fa384
Roman Njoroge <roman.njoroge@njuguna.com>
2022-07-07 15:11:48 +0300

    HTTP Module and a mini website that is hosted on my machine that's configured as a server

---
+++ http-module.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+const http = require('http')
+const { url } = require('inspector')
+
+/* Create a server - req is a request
+                     res is the server response
+*/
+const testHtmlCode = fs.readFileSync(
+    './test.html',
+    'utf-8'
+)
+
+const server = http.createServer((req, res)=>{
+    if (req.url === '/'){
+        res.end(`<h1>Welcome to our home page<h1>
+        <p>
+        <a href='/test'>Go to test page</a>
+        </p>
+        `)
+    }
+    else if (req.url === '/test'){
+        res.end(testHtmlCode)
+    }
+    res.end(`
+        <h1>Oops!</h1>
+        <p>We cant find the file your looking for</p>
+    `
+    )    
+})
+
+// The port the server listens to 
+server.listen(5000)
