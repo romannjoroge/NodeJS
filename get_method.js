b55cbf26781603f4e82d008b802f05dd05a0296d
Roman Njoroge <roman.njoroge@njuguna.com>
2022-07-13 01:26:13 +0300

    DELETE method and using Postman to test routes

--- get_method.js
+++ get_method.js
@@ -35,6 +35,45 @@ app.post('/api/people', (req, res)=>{
     res.status(400).json({success:false, msg:'please send form data'})
 })
 
+app.post('/api/postman/people', (req, res)=>{
+    const {name} = req.body
+    if (name){
+        return res.status(201).json({success: true, data: [...people, name]})
+    }
+    res.status(400).json({success: false, msg: 'please send data'})
+})
+
+app.put('/api/people/:id', (req, res)=>{
+    const {name} = req.body
+    const {id} = req.params
+    
+    const person = people.find((person)=>person.id === Number(id))
+
+    if (!person){
+        res.status(404)
+        return res.json({success: false, msg:`Person with id ${id} doesn't exist`})
+    }
+    const newPerson = people.map((person)=>{
+        if (person.id === Number(id)){
+            person.name = name
+        }
+        return person
+    })
+    res.status(201).json({success: true, data: newPerson})
+})
+
+app.delete('/api/people/:id', (req, res)=>{
+    const {id} = req.params
+    person = people.find((person)=>person.id === Number(id))
+
+    if (!person){
+        res.status(404)
+        return res.json({succes:false, msg:`Person with id ${id} doesn't exist`})
+    }
+    const newPeople = people.filter((person)=>person.id !== Number(id))
+    res.status(200).json({success:true, data:newPeople})
+})
+
 app.listen(5000, ()=>{
     console.log('Server is listening on port 5000...')
 })
